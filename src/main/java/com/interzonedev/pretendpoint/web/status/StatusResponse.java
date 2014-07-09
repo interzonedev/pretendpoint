60d10a78ada79dc9ce89f8ea2d43cc792150e886
Mark Markarian <mark@interzoendev.com>
2014-07-09 10:30:52 -0700

    Used version 2.1.3-SNAPSHOT of heroku-support. Added the current environment to the output of the status endpoint.

--- src/main/java/com/interzonedev/pretendpoint/web/status/StatusResponse.java
+++ src/main/java/com/interzonedev/pretendpoint/web/status/StatusResponse.java
@@ -1,11 +1,16 @@
 package com.interzonedev.pretendpoint.web.status;
 
+import com.interzonedev.herokusupport.environment.Environment;
 import com.interzonedev.pretendpoint.web.PretendPointResponse;
 
 public class StatusResponse extends PretendPointResponse {
 
     public long getCurrentTimeMillis() {
         return System.currentTimeMillis();
+    }
+
+    public String getCurrentEnvironment() {
+        return Environment.getCurrentEnvironment().getEnvironmentName();
     }
 
 }
